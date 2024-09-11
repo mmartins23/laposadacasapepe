8732db981124c3e77a2067fe74c7cd0c3f8b027c
Marlon Martins <marlon-martins@outlook.com>
2024-09-12 00:08:03 +0200

    La Posada Navbar

---
+++ laposada/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import { MdOutlineFoodBank, MdOutlineWhatsapp } from "react-icons/md";
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="container">
+            <span>
+                <MdOutlineFoodBank size={64} />
+            </span>
+            <ul>
+                <li>Historia</li>
+                <li>Recomendaciones</li>
+                <li>Menú</li>
+                <li>Equipo</li>
+                <li>Contacto</li>
+                <li><button className="btn"><MdOutlineWhatsapp size={18} color="green" />
+                </button></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
