e166886570f55b598a772352952a54b4685c7d1a
Marlon Martins <marlon-martins@outlook.com>
2024-09-18 12:40:27 +0200

    La Posada News Component

--- laposada/src/components/News/News.jsx
+++ laposada/src/components/News/News.jsx
@@ -1,34 +1,34 @@
 import './News.css';
-import news_1 from '../../assets/laposada_wine_resized.jpg';
-import news_2 from '../../assets/laposada_paella_resized.jpg';
-import news_3 from '../../assets/laposada_meat_resized.jpg';
-import { GiWineBottle, GiChickenOven, GiForkKnifeSpoon } from "react-icons/gi";
+import news_1 from '../../assets/laposada_jamon.jpg';
+import news_2 from '../../assets/laposada_paella.jpg';
+import news_3 from '../../assets/laposada_meat.jpg';
+import { GiMeat, GiChickenOven, GiForkKnifeSpoon } from "react-icons/gi";
 
 
 const News = () => {
   return (
     <div className='news'>
-    <div className='new'>
-      <img src={news_1} alt='Wine'/>
-      <div className='caption'>
-        <span><GiWineBottle size={36}/></span>
-        <p>Selección de Vinos Excepcionales</p>
+      <div className='new'>
+        <img src={news_1} alt='Jamon Serrano' />
+        <div className='caption'>
+          <span><GiMeat size={36} /></span>
+          <p>Sabor Inigualable del Jamón</p>
+        </div>
       </div>
-    </div>
-    <div className='new'>
-    <img src={news_2} alt='Paella'/>
-    <div className='caption'>
-        <span><GiForkKnifeSpoon size={36}/></span>
-        <p>Especialidad en Paella</p>
+      <div className='new'>
+        <img src={news_2} alt='Paella' />
+        <div className='caption'>
+          <span><GiForkKnifeSpoon size={36} /></span>
+          <p>Especialidad en Paella</p>
+        </div>
       </div>
-    </div>
-    <div className='new'>
-    <img src={news_3} alt='BBQ'/>
-    <div className='caption'>
-        <span><GiChickenOven size={36}/></span>
-        <p>Auténtico Sabor a la Brasa</p>
+      <div className='new'>
+        <img src={news_3} alt='BBQ' />
+        <div className='caption'>
+          <span><GiChickenOven size={36} /></span>
+          <p>Auténtico Sabor a la Brasa</p>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
