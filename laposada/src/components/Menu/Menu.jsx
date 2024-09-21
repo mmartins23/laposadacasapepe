a2b0144aed475d1924a4f9211fb8910f35244b1b
Marlon Martins <marlon-martins@outlook.com>
2024-09-21 15:19:59 +0200

    La Posada Menu Component

--- laposada/src/components/Menu/Menu.jsx
+++ laposada/src/components/Menu/Menu.jsx
@@ -6,7 +6,7 @@ const Menu = () => {
       <div className="menu-item">
         <h3>Menú Degustación</h3>
         <h4><sup>€</sup>28</h4>
-        <p>Con una bebida - 28€<br/>Con todas bebida (Incluye café) - 33€</p>
+        <p>Con una bebida - 28€<br />Con todas bebida (Incluye café) - 33€</p>
         <h5>ENTRANTES</h5>
         <p>Cuatro Deliciosos Entrantes de la casa | Tomate con capellán y olivas.</p>
         <h5>PLATO PRINCIPAL</h5>
@@ -16,7 +16,7 @@ const Menu = () => {
         <h5>BEBIDAS</h5>
         <p>Agua | Refresco | Cerveza | Vinos de la casa.</p>
       </div>
-      
+
       <div className="menu-item recommended">
         <h3>Menú Especial</h3>
         <h4><sup>€</sup>40</h4>
@@ -24,13 +24,13 @@ const Menu = () => {
         <h5>ENTRANTES</h5>
         <p>Cuencos con 3 salsas y pan gallego | Carpaccio de salmón | Alcachofa rellena (según temporada) | Huevos con bechamel | Gambas al ajillo | Ensalada de capellán.</p>
         <h5>PLATO A ELEGIR</h5>
-        <p>Cordero | Pollo | Churrasco de ternera | Secreto al romero | Costillar de cerdo con salsa BBQ | Solomillo al romero | | Pescados frescos. (Todo a la brasa acompañado de patatas fritas y pimientos de padrón)</p>
+        <p>Cordero | Pollo | Churrasco de ternera | Secreto al romero | Costillar de cerdo con salsa BBQ | Solomillo al romero | | Pescados frescos. (Todo a la brasa acompañado de patatas fritas y pimientos de padrón).</p>
         <h5>POSTRES</h5>
         <p>Postres caseros a elegir.</p>
         <h5>BEBIDAS</h5>
         <p>Agua | Refrescos | Cerveza | Vino Blanco, Rosado y Tinto R. Duero (1botella x 2 pers.) | Café.</p>
       </div>
-      
+
       <div className="menu-item">
         <h3>Menú de Arroz</h3>
         <h4><sup>€</sup>40</h4>
@@ -44,7 +44,7 @@ const Menu = () => {
         <h5>BEBIDAS</h5>
         <p>Agua | Refrescos | Cerveza | Vino Blanco, Rosado y Tinto R. Duero (1botella x 2 pers.) | Café.</p>
       </div>
-      
+
       <div className="menu-item recommended">
         <h3>Menú La Posada</h3>
         <h4><sup>€</sup>50</h4>
