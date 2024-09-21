186b8aaa7e73ff55f8b95c97e43a990da33b16a4
Marlon Martins <marlon-martins@outlook.com>
2024-09-21 12:11:17 +0200

    La Posada Navbar Component

--- laposada/src/components/Navbar/Navbar.jsx
+++ laposada/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import './Navbar.css';
 import { MdOutlineFoodBank, MdOutlineWhatsapp } from "react-icons/md";
 import { useState, useEffect } from "react";
+import { Link } from 'react-scroll';
+
 
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
@@ -12,16 +14,15 @@ const Navbar = () => {
     }, [])
 
     return (
-        <nav className={`container ${sticky? 'dark-nav' : ""}`}>
-            <span>
-                <MdOutlineFoodBank size={64} />
+        <nav className={`container ${sticky ? 'dark-nav' : ""}`}>
+            <span><Link to='Hero' smooth={true} offset={-260} duration={500}><MdOutlineFoodBank size={64} /></Link>
             </span>
             <ul>
-                <li>El Restaurante</li>
-                <li>Gastronomía</li>
-                <li>Menú</li>
+                <li><Link to='El Restaurante' smooth={true} offset={-260} duration={500}>El Restaurante</Link></li>
+                <li><Link to='Gastromonia' smooth={true} offset={-260} duration={500}>Gastronomía</Link></li>
+                <li><Link to='Menu' smooth={true} offset={-220} duration={500}>Menú</Link></li>
                 <li>Equipo</li>
-                <li>Contacto</li>
+                <li><Link to='Contacto' smooth={true} offset={-200} duration={500}>Contacto</Link></li>
                 <li><button className="btn">Whats App<MdOutlineWhatsapp size={18} color="green" />
                 </button></li>
             </ul>
