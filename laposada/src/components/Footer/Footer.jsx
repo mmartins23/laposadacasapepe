6f395cef4041a14d3182835aebf36a161e6a78bc
Marlon Martins <marlon-martins@outlook.com>
2024-09-23 11:21:59 +0200

    La Posada Footer Component

--- laposada/src/components/Footer/Footer.jsx
+++ laposada/src/components/Footer/Footer.jsx
@@ -1,22 +1,29 @@
 import './Footer.css';
-
 import { FaFacebookSquare, FaInstagramSquare, FaTripadvisor } from "react-icons/fa";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <div className="footer">
+            {/* Social Media Icons */}
             <div className="social-icons">
-                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="facebook">
+                {/* Facebook Link */}
+                <a href="https://www.facebook.com/p/Restaurante-La-Posada-100063636669626/" target="_blank" rel="noopener noreferrer" className="facebook">
                     <FaFacebookSquare />
                 </a>
-                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="instagram">
+
+                {/* Instagram Link */}
+                <a href="https://www.instagram.com/laposadacasapepe/" target="_blank" rel="noopener noreferrer" className="instagram">
                     <FaInstagramSquare />
                 </a>
-                <a href="https://tripadvisor.com" target="_blank" rel="noopener noreferrer" className="tripadvisor">
+
+                {/* Tripadvisor Link */}
+                <a href="https://www.tripadvisor.es/Restaurant_Review-g2102293-d6681221-Reviews-La_Posada-Abanilla.html" target="_blank" rel="noopener noreferrer" className="tripadvisor">
                     <FaTripadvisor />
                 </a>
             </div>
+
+            {/* Copyright text */}
             <p>© {currentYear} Copyright La Posada Casa Pepe. All Rights Reserved</p>
         </div>
     );
