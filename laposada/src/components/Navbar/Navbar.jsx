5fb6c8d7804f0e54a441470c049a5e825b3cebf4
Marlon Martins <marlon-martins@outlook.com>
2024-09-23 12:39:19 +0200

    La Posada Navbar Media Queries Component

--- laposada/src/components/Navbar/Navbar.jsx
+++ laposada/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import './Navbar.css';
 import { MdOutlineFoodBank, MdOutlineWhatsapp } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { useState, useEffect } from "react";
 import { Link } from 'react-scroll';
 
 
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -13,19 +15,24 @@ const Navbar = () => {
         })
     }, [])
 
+    const toogleMenu = () => {
+        mobileMenu? setMobileMenu(false) : setMobileMenu(true);
+    }
+
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ""}`}>
-            <span><Link to='Hero' smooth={true} offset={-260} duration={500}><MdOutlineFoodBank size={64} /></Link>
+            <span className='logo'><Link to='Hero' smooth={true} offset={-260} duration={500}><MdOutlineFoodBank size={64} /></Link>
             </span>
-            <ul>
+            <ul className={mobileMenu? '' : 'hide-mobile-menu'}>
                 <li><Link to='El Restaurante' smooth={true} offset={-260} duration={500}>El Restaurante</Link></li>
                 <li><Link to='Gastromonia' smooth={true} offset={-260} duration={500}>Gastronomía</Link></li>
                 <li><Link to='Menu' smooth={true} offset={-220} duration={500}>Menú</Link></li>
                 <li>Equipo</li>
                 <li><Link to='Contacto' smooth={true} offset={-200} duration={500}>Contacto</Link></li>
-                <li><button className="btn">Whats App<MdOutlineWhatsapp size={18} color="green" />
+                <li><button className="btn">Whats App<MdOutlineWhatsapp size={16} color="green" />
                 </button></li>
             </ul>
+            <span className='menu-icon' onClick={toogleMenu}><GiHamburgerMenu/></span>
         </nav>
     )
 }
