043e428ff417e82a7a7a0fddd663f7e0efaa5a97
Marlon Martins <marlon-martins@outlook.com>
2024-09-23 15:47:08 +0200

    La Posada Navbar Component

--- laposada/src/components/Navbar/Navbar.jsx
+++ laposada/src/components/Navbar/Navbar.jsx
@@ -16,23 +16,28 @@ const Navbar = () => {
     }, [])
 
     const toogleMenu = () => {
-        mobileMenu? setMobileMenu(false) : setMobileMenu(true);
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
     }
 
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ""}`}>
             <span className='logo'><Link to='Hero' smooth={true} offset={-260} duration={500}><MdOutlineFoodBank size={64} /></Link>
             </span>
-            <ul className={mobileMenu? '' : 'hide-mobile-menu'}>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
                 <li><Link to='El Restaurante' smooth={true} offset={-260} duration={500}>El Restaurante</Link></li>
                 <li><Link to='Gastromonia' smooth={true} offset={-260} duration={500}>Gastronomía</Link></li>
                 <li><Link to='Menu' smooth={true} offset={-220} duration={500}>Menú</Link></li>
-                <li>Equipo</li>
+                <li><Link to='Equipo' smooth={true} offset={-220} duration={500}>Equipo</Link></li>
                 <li><Link to='Contacto' smooth={true} offset={-200} duration={500}>Contacto</Link></li>
-                <li><button className="btn">Whats App<MdOutlineWhatsapp size={16} color="green" />
-                </button></li>
+                <li>
+                    <a href="https://wa.me/34653838263" target="_blank" rel="noopener noreferrer">
+                        <button className="btn">
+                            Whats App<MdOutlineWhatsapp size={16} color="green" />
+                        </button>
+                    </a>
+                </li>
             </ul>
-            <span className='menu-icon' onClick={toogleMenu}><GiHamburgerMenu/></span>
+            <span className='menu-icon' onClick={toogleMenu}><GiHamburgerMenu /></span>
         </nav>
     )
 }
